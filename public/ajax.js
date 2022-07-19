37afcb9b5ba5036fa4d5032619b3f4032556c642
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-07-19 14:30:39 +0700

    refactor code

--- public/ajax.js
+++ public/ajax.js
@@ -46,20 +46,27 @@ $('#new-todo-form').submit(function(e) {
         $('#todo-list').append(
             `
             <li class="list-group-item">
+            <form action="/todos/${data._id}" method="POST" class="edit-item-form">
+						<div class="form-group">
+							<label for="${data._id}">Item Text</label>
+							<input type="text" value="${data.text}" name="todo[text]" class="form-control" id="${data.text}">
+						</div>
+						<button class="btn btn-primary">Update Item</button>
+					</form>
 					<span class="lead">
 						${data.text}
 					</span>
 					<div class="pull-right">
-						<a href="/todos/${data._id}/edit" class="btn btn-sm btn-warning">Edit</a>
-						<form style="display: inline" method="POST" action="/todos/${data._id}">
-							<button type="submit" class="btn btn-sm btn-danger">Delete</button>
-						</form>
+                    <button class="btn btn-sm btn-warning edit-button">Edit</button>
+                    <form style="display: inline" method="POST" action="/todos/<%=todo._id%>" class="delete-item-form">
+                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                    </form>
 					</div>
 					<div class="clearfix"></div>
 				</li>
             `
         )
-        $('#new-todo-form').find('.for,m-control').val('')
+        $('#new-todo-form').find('.form-control').val('')
     })
 })
 
@@ -82,8 +89,8 @@ $('#todo-list').on('submit','.edit-item-form',function(e) {
                 `
                 <form action="/todos/${data._id}" method="POST" class="edit-item-form">
 						<div class="form-group">
-							<label>Item Text</label>
-							<input type="text" value="${data.text}" name="todo[text]" class="form-control">
+                        <label for="${data._id}">Item Text</label>
+                        <input type="text" value="${data.text}" name="todo[text]" class="form-control" id="${data.text}">
 						</div>
 						<button class="btn btn-primary">Update Item</button>
 					</form>
