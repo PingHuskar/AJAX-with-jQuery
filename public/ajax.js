d9c58f9d3781d42d2fef0349bce4d1c0beeda645
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-07-19 10:29:07 +0700

    CRUD

--- public/ajax.js
+++ public/ajax.js
@@ -1,3 +1,40 @@
-$.get('/todos', function(data) {
-    debugger
+// $.get('/todos', function(data) {
+//     // debugger
+//     console.log(data)
+// })
+
+// $('form').submit(function(e) {
+//     e.preventDefault();
+//     var formData = $(this).serialize();
+//     // .serialize() pull all input values from the form to string
+//     $.post('/todos', formData,function(data) {
+//         console.log(data);
+//     })
+// })
+// $('form').submit(function(e) {
+//     e.preventDefault();
+//     var formData = $(this).serialize();
+//     // .serialize() pull all input values from the form to string
+//     var formAction = $(this).attr('action');
+//     $.ajax({
+//         url: formAction,
+//         data: formData,
+//         type: 'PUT',
+//         success: function(data) {
+//             debugger
+//         }
+//     })
+// })
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    // .serialize() pull all input values from the form to string
+    var formAction = $(this).attr('action');
+    $.ajax({
+        url: formAction,
+        type: 'DELETE',
+        success: function(data) {
+            debugger
+        }
+    })
 })
